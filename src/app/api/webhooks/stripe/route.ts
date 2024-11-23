ba2e6eb7bb3c4bbf11517a6400dd91113f82b527
Florian Heysen <heysen.florian@gmail.com>
2024-11-23 23:26:32 +0100

    fix stripe

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,4 +1,3 @@
-import { stripe } from "@/app/lib/stripe"
 import { db } from "@/db"
 import { headers } from "next/headers"
 import Stripe from "stripe"
@@ -6,6 +5,11 @@ import Stripe from "stripe"
 export async function POST(req: Request) {
   const body = await req.text()
   const signature = headers().get("stripe-signature")
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+    apiVersion: "2024-11-20.acacia",
+    typescript: true,
+  })
 
   const event = stripe.webhooks.constructEvent(
     body,
